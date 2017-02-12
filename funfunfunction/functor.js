14db14e8000e97bc4cb7991ed8185be54301e25f
jk <vargsmane@gmx.de>
2017-02-12 19:07:29 +0100

    Linting

--- funfunfunction/functor.js
+++ funfunfunction/functor.js
@@ -1,5 +1,8 @@
+"use strict";
+
+// not really a functor since it's not generic!
 function stringFunctor(value, fn) {
-	return value.split("").map(char => String.fromCharCode(fn(char.charCodeAt(0)))).join("");
+    return value.split("").map((char) => String.fromCharCode(fn(char.charCodeAt(0)))).join("");
 }
 
-console.log(stringFunctor("ABC", value => value + 1));
+console.log(stringFunctor("ABC", (value) => value + 1));
